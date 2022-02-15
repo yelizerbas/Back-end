d733d63fb51add96b23d2547652eb7b82ad35ce9
yelizerbas <94406320+yelizerbas@users.noreply.github.com>
2022-02-15 20:41:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hellow World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
