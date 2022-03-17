1b176b84213b032fa37fcc3eb11f65179d201a61
yelizerbas <yeliz.erbas@hotmail.com>
2022-03-17 22:59:55 +0100

    css

---
+++ static/script/script.js
@@ -0,0 +1,13 @@
+// function hamburgermenu
+
+// const menuButton = document.getElementById("knoppie")
+// const mainElement = document.querySelector(`main`);
+
+
+//  menuButton.addEventListener("click", () => {
+//   mainElement.classList.add(`showMenu`);
+// });
+
+// crossButton.addEventListener("click", () => {
+//   mainElement.classList.remove(`showMenu`);
+// });
