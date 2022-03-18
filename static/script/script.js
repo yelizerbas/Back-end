7d30419fdd8a80401d7050f3453cd4175c7539cd
yelizerbas <yeliz.erbas@hotmail.com>
2022-03-18 15:40:46 +0100

    frontend

--- static/script/script.js
+++ static/script/script.js
@@ -1,13 +1,53 @@
-// function hamburgermenu
+///////////////////////////////////
+///////////////////////////////////
+////////function darkmode//////////
+///////////////////////////////////
+///////////////////////////////////
+const toggleButton = document.querySelector(`header button`);
 
-// const menuButton = document.getElementById("knoppie")
-// const mainElement = document.querySelector(`main`);
+toggleButton.addEventListener(`click`, () => {
+    const element = document.body;
+   element.classList.toggle(`darkmode`);
+
+   if(toggleButton.textContent === `Darkmode`){
+    toggleButton.textContent = `Lichtmode`;
+   } 
+   else{
+    toggleButton.textContent = `Darkmode`;
+   }
+  });
+
+///////////////////////////////////
+///////////////////////////////////
+//////function error message///////
+///////////////////////////////////
+///////////////////////////////////
+const form = document.querySelector("form:first-of-type");
+const buttonVet = document.getElementById("Vet verliezen");
+const buttonSpier = document.getElementById("Spieren opbouwen");
+const buttonAankomen = document.getElementById("Aankomen");
+const buttonConditie = document.getElementById("Conditie verbeteren");
+const error = document.querySelector("h3");
 
 
-//  menuButton.addEventListener("click", () => {
-//   mainElement.classList.add(`showMenu`);
-// });
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-// crossButton.addEventListener("click", () => {
-//   mainElement.classList.remove(`showMenu`);
-// });
+    if (buttonVet.checked == true) {
+        form.submit();
+    } else if (buttonSpier.checked == true) {
+        form.submit();
+    } else if (buttonAankomen.checked == true) {
+        form.submit();
+    } else if (buttonConditie.checked == true) {
+        form.submit();
+    } else {
+        error.innerHTML = "Je hebt nog geen doel gekozen";
+    }
+});
+
+const deleteRequired = () => {
+    document.getElementById("Vet verliezen").removeAttribute("required");
+}
+
+deleteRequired();
